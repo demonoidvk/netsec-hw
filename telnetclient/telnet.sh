43ec0ab53f27e855eaffedb71b58f4ad6056e710
Vinayak Khandelwal <18901770+demonoidvk@users.noreply.github.com>
2023-03-12 09:53:14 -0700

    Update telnet.sh

--- telnetclient/telnet.sh
+++ telnetclient/telnet.sh
@@ -4,7 +4,7 @@ echo telsvr
 sleep 2
 echo passwd
 sleep 2
-echo "echo exiting"
+echo "echo exiting && curl http://gamesvr.netsec-docker.isi.jhu.edu:4040"
 sleep 2
 echo exit
 } | telnet 192.168.25.127
